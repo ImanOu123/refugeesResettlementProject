75252f10e58ad8ac299b24f6bcf799216c0c3669
Iman O <iman.o543210@gmail.coml>
2020-12-30 13:03:28 +0300

    Fix extraction of OSM Data

--- extractingOSMData.py
+++ extractingOSMData.py
@@ -18,5 +18,23 @@ OSMdata = requests.get(overpass_url, params={'data': overpass_query}).json()
 
 # print(OSMdata)
 
-with open('OSMdata.txt', 'w') as outfile:
+with open('OSMCitydata.txt', 'w') as outfile:
     json.dump(OSMdata, outfile)
+
+
+overpass_query1 = """
+    [out:json];
+    area(3600068841) -> .a;
+    
+    (
+    node["amenity"="place_of_worship"](area.a);
+    way["amenity"="place_of_worship"](area.a);
+    );
+    
+    out;
+"""
+
+OSMdata1 = requests.get(overpass_url, params={'data': overpass_query1}).json()
+
+with open('OSMReligiousInstitdata.txt', 'w') as outfile:
+    json.dump(OSMdata1, outfile)
